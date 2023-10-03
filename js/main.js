7d7c64882f082de923d7c79c6b63884078c2af70
6ix9inger <florian.neuninger@sz-ybbs.ac.at>
2023-10-03 18:02:23 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//opacity wird mit dem Scroll Wert kontrolliert
+window.addEventListener(`scroll`, function (e) {
+    const scroll = this.scrollY / 250;
+    document.getElementById("blurred").style.opacity = `${scroll}`;
+  });
+
